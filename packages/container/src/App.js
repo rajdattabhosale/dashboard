277710133eac2c7db48b29ad755e43b394c5f4d4
rajdatta <rajdatta.bhosale@gmail.com>
2022-03-01 22:27:14 +0000

    some

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-const history = createBrowserHistory();
+function App() {
+  return (
+    <div>
+      <p>Some more changes this stuff works coming here!!!</p>
+      <header>
+        <p>
+          Edit <code>src/App.js</code>
+        </p>
 
-const Header = () => (
-  <div>
-    <title>Dashboard</title>
-    <Link to="/">Home</Link>
-    <br />
-    <Link to="/react">MFE - React App</Link>
-    <br />
-    <Link to="/vue">MFE - Vue App</Link>
-  </div>
-);
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          learn some more good stuff
+        </a>
+      </header>
+    </div>
+  );
+}
 
-export default () => {
-  return <Header />;
-};
+export default App;
