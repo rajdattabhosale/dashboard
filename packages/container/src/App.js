91c97dabdfff7251cf9987db6043a492368d6c80
rajdatta <rajdatta.bhosale@gmail.com>
2022-03-01 17:38:48 +0000

    some

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,3 +1,5 @@
+import logo from "./logo.svg";
+import "./App.css";
 import React from "react";
 import HelloReactApp from "./components/HelloReactApp";
 import { Route, Switch, Router, Link } from "react-router-dom";
@@ -8,6 +10,22 @@ const history = createBrowserHistory();
 
 const Header = () => (
   <div>
+    <p>Some more changes coming here!!!</p>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code>
+      </p>
+
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        learn some more good stuff
+      </a>
+    </header>
     <title>Dashboard</title>
     <Link to="/">Home</Link>
     <br />
