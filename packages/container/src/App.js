ed6e02b9d7a039270a837d6a28a44da8c38cad88
rajdatta <rajdatta.bhosale@gmail.com>
2022-03-01 17:49:07 +0000

    some

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,47 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
-/*
-import HelloReactApp from "./components/HelloReactApp";
-import { Route, Switch, Router, Link } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import HelloVueApp from "./components/HelloVueApp";
-**/
 
 const history = createBrowserHistory();
 
 const Header = () => (
   <div>
-    <p>Some more changes coming here!!!</p>
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code>
-      </p>
-
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        learn some more good stuff
-      </a>
-    </header>
     <title>Dashboard</title>
     <Link to="/">Home</Link>
     <br />
     <Link to="/react">MFE - React App</Link>
     <br />
-    <Link to="/vue">MFE - xVue App</Link>
+    <Link to="/vue">MFE - Vue App</Link>
   </div>
 );
 
 export default () => {
-  return (
-    <Router history={history}>
-      <Header />
-      <hr />
-    </Router>
-  );
+  return <Header />;
 };
