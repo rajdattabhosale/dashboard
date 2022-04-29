7c48bb360cf8bb9b59d083658b118db900a71678
rajdatta <rajdatta.bhosale@gmail.com>
2022-04-29 13:57:05 +0100

    some

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HelloReactApp from "./components/HelloReactApp";
-import { Route, Switch, Router, Link } from "react-router-dom";
+import { Route, Switch, Router, Link, PrivateRoute } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import HelloVueApp from "./components/HelloVueApp";
 
@@ -14,6 +14,8 @@ const Header = () => (
     <Link to="/react">MFE React App</Link>
     <br />
     <Link to="/vue">MFE Vue App</Link>
+    <br />
+    <Link to="/app1">App1</Link>
   </div>
 );
 
@@ -26,6 +28,12 @@ export default () => {
         <Route path="/vue" component={HelloVueApp} />
         <Route path="/react" component={HelloReactApp} />
         <Route path="/" component={HelloReactApp} />
+        <Route path="/app1" />
+        <PrivateRoute
+          path="/admin"
+          component={HelloReactApp}
+          isAuthenticated={true}
+        />
       </Switch>
     </Router>
   );
