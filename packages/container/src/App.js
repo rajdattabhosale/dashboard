d65620f261dc9f7c8884d42f81eb83ed24a5b35a
rajdatta <rajdatta.bhosale@gmail.com>
2022-04-29 13:46:12 +0100

    some

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -9,6 +9,7 @@ const history = createBrowserHistory();
 const Header = () => (
   <div>
     <title>Dashboard</title>
+    <h1>Menu</h1>
     <Link to="/">Home</Link>
     <br />
     <Link to="/react">MFE React App</Link>
